9de7d4cca9e43d5aa57e0a1abed021fd2dfdb966
Sergio <sergioforerop@hotmail.com>
2015-02-10 23:23:52 -0500

    primera prueba

--- src/main/java/edu/uniandes/ecos/Main.java
+++ src/main/java/edu/uniandes/ecos/Main.java
@@ -1,7 +1,7 @@
 package edu.uniandes.ecos;
 
-
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,23 +15,36 @@ import org.eclipse.jetty.servlet.ServletHolder;
  *
  */
 public class Main extends HttpServlet {
-    public static String texto= "";
+
+    public static String texto = "";
+
     public static void main(String[] args) throws Exception {
-//        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
-//        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-//        context.setContextPath("/");
-//        server.setHandler(context);
-//        context.addServlet(new ServletHolder(new Main()), "/*");
-//        server.start();
-//        server.join();
-        
+        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(new Main()), "/*");
+        server.start();
+        server.join();
     }
-    
-   @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
-      showHome(req,resp);
-  }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.print("<html>\n"
+                + "<body>\n"
+                + "<form action=\"HelloForm\" method=\"GET\">\n"
+                + "First Name: <input type=\"text\" name=\"first_name\">\n"
+                + "<br />\n"
+                + "Last Name: <input type=\"text\" name=\"last_name\" />\n"
+                + "<input type=\"submit\" value=\"Submit\" />\n"
+                + "</form>\n"
+                + "</body>\n"
+                + "</html>");
+
+        showHome(req, resp);
+    }
 
     private void showHome(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
